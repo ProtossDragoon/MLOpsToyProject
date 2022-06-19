b75bed28cf1b3b18b5c87e358bf1ce22c87ce0af
ProtossDragoon <dlwkdgn1@naver.com>
2022-06-19 14:26:41 +0900

    feat: 실제 데이터 일부와 최소주의 모델링
    - LSTM 기반 모델링
    - 원핫 인코딩 전처리 기능 추가

---
+++ src/model/custom/models/lstm.py
@@ -0,0 +1,114 @@
+# 서드파티
+import tensorflow as tf
+import numpy as np
+
+# 프로젝트
+from src.preprocessing.sms import SMSDataPreprocessingManager
+
+
+class LSTMModel(tf.keras.Model):
+
+    def __init__(self,
+                 word_embedding_dim: int = None,
+                 sentence_max_len_dim: int = None,
+                 output_dim: int = 2):
+        super(LSTMModel, self).__init__()
+        self.word_embedding_dim = word_embedding_dim
+        self.sentence_max_len_dim = sentence_max_len_dim
+        self.output_dim = output_dim
+        self.l1 = tf.keras.layers.LSTM(80, return_sequences=True)
+        self.l2 = tf.keras.layers.LSTM(30, return_sequences=True)
+        self.l3 = tf.keras.layers.LSTM(output_dim, activation='softmax')
+
+    def call(self, x, training=None):
+        x = self.l1(x)
+        x = self.l2(x)
+        x = self.l3(x)
+        return x
+
+    @staticmethod
+    def loss_object(y, y_hat):
+        scce = tf.keras.losses.SparseCategoricalCrossentropy()
+        return scce(y, y_hat)
+
+
+def train_step(model, x, y, train_acc):
+    adam = tf.keras.optimizers.Adam()
+    with tf.GradientTape() as tape:
+        y_hat = model(x, training=True)
+        loss = model.loss_object(y, y_hat)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        adam.apply_gradients(zip(gradients, model.trainable_variables))
+    print(f'train_accuracy: {train_acc(y, y_hat):.3f}')
+
+
+def test_step(model, text, label, test_acc):
+    y_hat = model(text, training=False)
+    print(f'test_accuracy: {test_acc(label, y_hat):.3f}')
+
+
+def main():
+    # 데이터 로드
+    prep_manager = SMSDataPreprocessingManager(
+        feature_column_name='message',
+        label_column_name='label'
+    )
+    path = './data/sample/spam.csv'
+    df = prep_manager.read_sample_data(path)
+
+    # 기본 전처리
+    prep_manager.remove_stopwords(df)
+    prep_manager.sentence_to_lowercase(df)
+    train_df, test_df = prep_manager.split(df, 0.8)
+
+    # 훈련 데이터 전처리
+    train_x, train_y = prep_manager.get_xy(train_df, {'spam': 0., 'ham': 1.})
+    train_x_onehot = prep_manager.get_onehot(train_x)
+
+    # 테스트 데이터 전처리
+    test_x, test_y = prep_manager.get_xy(test_df, {'spam': 0., 'ham': 1.})
+    test_x_onehot = prep_manager.get_onehot(test_x)
+
+    word_embedding_dim = train_x_onehot.shape[-1]
+    sentence_max_len_dim = train_x_onehot.shape[-2]
+    model = LSTMModel(
+        word_embedding_dim=word_embedding_dim,
+        sentence_max_len_dim=sentence_max_len_dim,
+    )
+
+    epochs = 3
+    batch_size = 2
+
+    print(f'train data spec:')
+    print(f'x: {train_x_onehot.shape}({train_x_onehot.dtype})')
+    print(f'y: {train_y.shape}({train_y.dtype})')
+    print(f'test data spec:')
+    print(f'x: {test_x_onehot.shape}({test_x_onehot.dtype})')
+    print(f'y: {test_y.shape}({test_y.dtype})')
+
+    tfds_train = tf.data.Dataset.from_tensor_slices(
+        (train_x_onehot, train_y)).batch(batch_size)
+    tfds_test = tf.data.Dataset.from_tensor_slices(
+        (test_x_onehot, test_y)).batch(1)
+
+    # 메트릭 정의
+    train_acc = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accy')
+    test_acc = tf.keras.metrics.SparseCategoricalAccuracy(name='test_acc')
+
+    # 학습
+    for epoch in range(1, epochs+1):
+        for step, (train_x, train_y) in enumerate(tfds_train, 1):
+            print(f'epoch: {epoch}, step: {step}', end='\t')
+            # print(f'x: {repr(train_x.shape)}, y: {repr(train_y.shape)}')
+            train_step(model, train_x, train_y, train_acc=train_acc)
+
+    # 평가
+    for step, (test_x, test_y) in enumerate(tfds_test, 1):
+        test_step(model, test_x, test_y, test_acc=test_acc)
+
+    # 요약
+    print(f'train_acc: {train_acc.result():.3f}, test_acc: {test_acc.result():.3f}')
+
+
+if __name__ == '__main__':
+    main()
--- src/preprocessing/sms.py
+++ src/preprocessing/sms.py
@@ -6,6 +6,9 @@ import nltk
 from nltk.corpus import stopwords
 import numpy as np
 import pandas as pd
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
@@ -14,6 +17,7 @@ from src.preprocessing.common import PreprocessingManager
 
 
 class SMSDataPreprocessingManager(PreprocessingManager):
+
     def __init__(
         self,
         feature_column_name: str = 'message',
@@ -82,13 +86,37 @@ class SMSDataPreprocessingManager(PreprocessingManager):
         if label_map:
             self.label_map = label_map
             for key, val in label_map.items():
-                df.loc[df[self.label_column_name] == key, self.label_column_name] = val
+                condition = (df[self.label_column_name] == key)
+                df.loc[condition, self.label_column_name] = val
         return (df[self.feature_column_name], df[self.label_column_name].astype(np.float32))
+
+    def get_onehot(
+        self,
+        x: pd.Series,
+    ) -> np.ndarray:
+        if not hasattr(self, 'onehot_encoder'):
+            self.onehot_encoder = Tokenizer()
+            self.onehot_encoder.fit_on_texts(x)
+            self.cnt_unique_words = len(self.onehot_encoder.word_counts)
+            self.sentence_max_len = None
+            print(f'{self.cnt_unique_words}-kind of words were detected.')
+
+        def encode(x):
+            encoded = self.onehot_encoder.texts_to_sequences(x)
+            if self.sentence_max_len is None:
+                self.sentence_max_len = max([len(s) for s in encoded])
+                print(f'Maximum length of sentence: {self.sentence_max_len}')
+            padded = pad_sequences(encoded, maxlen=self.sentence_max_len)
+            #NOTE: Tokenizer reserves 0 as an "out of scope" index
+            return to_categorical(padded, num_classes=self.cnt_unique_words+1)
+        x_onehot = encode(x)
+        print(f'One-hot matrix shape: {x_onehot.shape}')
+        return x_onehot
 
     def get_dtm(
         self,
-        x: np.array,
-    ):
+        x: pd.Series,
+    ) -> np.ndarray:
         if not hasattr(self, 'count_vectorizer'):
             self.count_vectorizer = CountVectorizer()
             self.count_vectorizer.fit(x)
@@ -98,8 +126,8 @@ class SMSDataPreprocessingManager(PreprocessingManager):
 
     def get_tfidf(
         self,
-        x_dtm,
-    ):
+        x_dtm: np.ndarray,
+    ) -> np.ndarray:
         if not hasattr(self, 'tfidf_transformer'):
             self.tfidf_transformer = TfidfTransformer()
             self.tfidf_transformer.fit(x_dtm)
